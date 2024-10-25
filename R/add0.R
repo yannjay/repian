1b2bfa14cc6271366cf7e6d52a61616c24f66cd7
yanglongjian888 <ylj520zh@163.com>
2024-10-26 02:00:18 +0800

    complete a tiny demo package flow

--- R/add0.R
+++ R/add0.R
@@ -1,1 +1,13 @@
-add0 <- function(a, b) a + b
+#' add two numbers
+#'
+#' @param a numeric
+#' @param b numeric
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' add0(2,5)
+add0 <- function(a, b) {
+  a + b
+}
