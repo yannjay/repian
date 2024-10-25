b795a84685aacce0b1313d0e1e55a3708be0ba9c
yanglongjian888 <ylj520zh@163.com>
2024-10-26 02:55:33 +0800

    adding another function

---
+++ R/regtest.R
@@ -0,0 +1,12 @@
+#' test if a atomic vector endwith "le"
+#'
+#' @param s an character atomic vector
+#'
+#' @return logical
+#' @export
+#'
+#' @examples
+#' regtest("apple")
+regtest <- function(s){
+  stringr::str_detect(s,"^*le$")
+}
